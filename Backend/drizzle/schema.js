129d9b256b6b149cac63d28da7f3b87ed19a1651
Satish Raut <satshraut@gmail.com>
2026-03-15 00:48:15 +0530

    The users Table Schema Defined using Drizzle

--- Backend/drizzle/schema.js
+++ Backend/drizzle/schema.js
@@ -1,10 +1,21 @@
 import { int, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
 
+// Schema for URL table
 export const urlTable = mysqlTable("shortLinks", {
   id: int("id").primaryKey().autoincrement(),
   shortCode: varchar("short_code", { length: 225 }).unique().notNull(),
   url: varchar("url", { length: 500 }).notNull(),
   clicks: int("clicks").default(0),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
+});
+
+// Schema for Users data table
+export const userTable = mysqlTable("users", {
+  id: int("id").autoincrement().primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
 });
